fc79c739428a381599bcaff8c7be68a66a14132b
anuragSRef <anurag@refactorq.com>
2023-11-22 12:24:23 +0530

    object the basics

--- Objects.js
+++ Objects.js
@@ -1,7 +1,4 @@
-//#Object the basics
-
 // Ordered like an object
-
 // integer properties are sorted, others appear in creation order.
 
 let codes = {
@@ -37,8 +34,7 @@ alert(user.name); // still John in the original object
 
 //##shallow copy and deep copy
 
-// here its shallow copy
-// Nested cloning
+// Nested cloning or  shallow copy
 let users = {
   name: "John",
   sizes: {
@@ -88,3 +84,149 @@ alert(user2.sizes === clone.sizes); // false, different objects
 // user and clone are totally unrelated now
 user2.sizes.width = 60; // change a property from one place
 alert(clone.sizes.width); //
+
+//Object methods, "this"
+
+let user3 = {
+  name: "John",
+  age: 30,
+
+  sayHi() {
+    // "this" is the "current object"
+    alert(this.name);
+  },
+};
+
+user3.sayHi(); // John
+
+// Here during the execution of user.sayHi(), the value of this will be user.
+
+// Technically, it’s also possible to access the object without this, by referencing it via the outer variable:
+
+let user4 = {
+  name: "John",
+  age: 30,
+
+  sayHi() {
+    alert(user4.name); // "user" instead of "this"
+  },
+};
+
+// …But such code is unreliable. If we decide to copy user to another variable, e.g. admin = user and overwrite user with something else, then it will access the wrong object.
+
+// That’s demonstrated below:
+
+let user5 = {
+  name: "John",
+  age: 30,
+
+  sayHi() {
+    alert(user5.name); // leads to an error
+  },
+};
+
+let admin = user5;
+user5 = null; // overwrite to make things obvious
+
+admin.sayHi(); // TypeError: Cannot read property 'name' of null
+// If we used this.name instead of user.name inside the alert, then the code would work.
+
+// #Constructor, operator "new"
+
+// Constructor function
+
+function User(name) {
+  this.name = name;
+  this.isAdmin = false;
+}
+
+let user6 = new User("Jack");
+
+alert(user6.name); // Jack
+alert(user6.isAdmin); // false
+
+// Inside a function, we can check whether it was called with
+// new or without it, using a special new.target property.
+
+// Methods in constructor
+function User(name) {
+  this.name = name;
+
+  this.sayHi = function () {
+    alert("My name is: " + this.name);
+  };
+}
+
+let john = new User("John");
+
+john.sayHi(); // My name is: John
+
+/*
+john = {
+   name: "John",
+   sayHi: function() { ... }
+}
+*/
+
+//#Symbols
+
+let id = Symbol();
+
+// For instance, here are two symbols with the same description – they are not equal
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+
+alert(id1 == id2); // false
+
+// Symbols in an object literal
+let ids = Symbol("id");
+
+let user7 = {
+  name: "John",
+  [id]: 123, // not "id": 123
+};
+
+// Symbol.keyFor
+// get symbol by name
+let sym = Symbol.for("name");
+let sym2 = Symbol.for("id");
+
+// get name by symbol
+alert(Symbol.keyFor(sym)); // name
+alert(Symbol.keyFor(sym2)); // id
+
+// The Symbol.keyFor internally uses the global symbol registry to look up the key for the symbol.
+// So it doesn’t work for non-global symbols. If the symbol is not global, it won’t be able to find
+//  it and returns undefined.
+
+let globalSymbol = Symbol.for("name");
+let localSymbol = Symbol("name");
+
+alert(Symbol.keyFor(globalSymbol)); // name, global symbol
+alert(Symbol.keyFor(localSymbol)); // undefined, not global
+
+alert(localSymbol.description); // name
+
+// Symbol.toPrimitive
+
+// obj[Symbol.toPrimitive] = function(hint) {
+// here goes the code to convert this object to a primitive
+// it must return a primitive value
+// hint = one of "string", "number", "default"
+// }
+
+//  user object implements it:
+let user8 = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  },
+};
+
+// conversions demo:
+alert(user8); // hint: string -> {name: "John"}
+alert(+user8); // hint: number -> 1000
+alert(user8 + 500); // hint: default -> 1500
--- javascriptFundamentals.js
+++ javascriptFundamentals.js
@@ -1,24 +1,3 @@
-// JavaScript Fundamentals
-
-// Hello, world!
-// Code structure
-// The modern mode, "use strict"
-// Variables
-// Data types
-// Interaction: alert, prompt, confirm
-// Type Conversions
-// Basic operators, maths
-// Comparisons
-// Conditional branching: if, '?'
-// Logical operators
-// Nullish coalescing operator '??'
-// Loops: while and for
-// The "switch" statement
-// Functions
-// Function expressions
-// Arrow functions, the basics
-// JavaScript specials
-
 // #Nullish coalescing operator '??'
 
 // The nullish coalescing operator is written as two question marks ??.
